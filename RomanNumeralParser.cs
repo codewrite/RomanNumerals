e0ded96e2446d627788ec119225db056adf91881
Jon Nethercott <jon@codewrite.co.uk>
2013-04-11 18:57:10 +0100

    Refactor - remove duplicate code
    Part of the refactoring meant that I created tables for the roman
    letters and arabic equivalent numbers. Two for loops were added to allow
    the duplicate if blocks to be merged.

--- RomanNumeralParser.cs
+++ RomanNumeralParser.cs
@@ -2,6 +2,9 @@
 {
     public static class RomanNumeralParser
     {
+        private static char[] romanLetters = { 'I', 'V', 'X', 'L', 'C' };
+        private static int[] arabicNumbers = { 1, 5, 10, 50, 100 };
+
         public static int RomanToArabic(this string romanNumeral)
         {
             var runningTotal = 0;
@@ -9,34 +12,37 @@
             for (int i = 0; i < romanNumeral.Length; i++)
             {
                 var symbol = romanNumeral[i];
-                if (symbol == 'I')
+
+                for (int j = 0; j < romanLetters.Length; j++)
                 {
-                    if (MoreCharacters(romanNumeral, i) && romanNumeral[i + 1] != 'I')
+                    if (symbol == romanLetters[j])
                     {
-                        runningTotal -= 1;
-                    }
-                    else
-                    {
-                        runningTotal += 1;
+                        bool needToAdd = false;
+                        if (MoreCharacters(romanNumeral, i))
+                        {
+                            for (int k = 0; k <= j; k++)
+                            {
+                                if (romanLetters[k] == romanNumeral[i + 1])
+                                {
+                                    needToAdd = true;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                            needToAdd = true;
+
+                        if (needToAdd)
+                        {
+                            runningTotal += arabicNumbers[j];
+                        }
+                        else
+                        {
+                            runningTotal -= arabicNumbers[j];
+                        }
+                        break;
                     }
                 }
-                else if (symbol == 'V')
-                    runningTotal += 5;
-                else if (symbol == 'X')
-                {
-                    if (MoreCharacters(romanNumeral, i) && (romanNumeral[i + 1] == 'L' || romanNumeral[i + 1] == 'C'))
-                    {
-                        runningTotal -= 10;
-                    }
-                    else
-                    {
-                        runningTotal += 10;
-                    }
-                }
-                else if (symbol == 'L')
-                    runningTotal += 50;
-                else if (symbol == 'C')
-                    runningTotal += 100;
             }
 
             return runningTotal;
